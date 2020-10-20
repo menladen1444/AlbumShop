594540055e9938d54a4fc83cc15572ff2e8fb11e
Huynh Le Minh Thuan <huynhle.minhthuan2131999@gmail.com>
2020-10-20 10:54:13 +0700

    add album model

---
+++ AlbumShop/Models/Album.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumShop.Models
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumID { get; set; }
+        public string TenAlbum { get; set; }
+        public string HinhAnh { get; set; }
+        public DateTime NgayPhatHanh { get; set; }
+        public double GiaBan { get; set; }
+        public int SoLuong { get; set; }
+        public int TacGiaID { get; set; }
+        public int DaBan { get; set; }
+        public double DiemDanhGia { get; set; }
+        public string XuatXu { get; set; }
+        public string MoTa { get; set; }
+        public int TheLoaiID { get; set; }
+    }
+}
